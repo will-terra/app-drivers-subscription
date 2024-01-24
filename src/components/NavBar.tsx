bde6aa89ebc584de9c055aa61bafdac9464b2908
will.terra <wpterra@outlook.com>
2024-01-23 22:31:11 -0300

    feat: navBar acessibility improved

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,27 +1,48 @@
-import { AppBar, Link, List, Toolbar } from "@mui/material";
+import {
+  AppBar,
+  Link,
+  List,
+  ListItem,
+  Tab,
+  Tabs,
+  Toolbar,
+} from "@mui/material";
 // import "@fontsource/roboto/400.css";
 import React from "react";
 
 const NavBar = () => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
   return (
     <AppBar data-testid="NavBar" sx={{ background: "#242424" }}>
       <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
         {" "}
         <img src="Logo.svg" alt="My ride logo"></img>{" "}
-        <List sx={{ display: "flex", gap: 5, fontFamily: "Roboto" }}>
-          <Link sx={{ color: "#FBA403" }} href="#" underline="none">
+        <Tabs value={value} onChange={handleChange} aria-label="Navigation Links"
+  role="navigation">
+          <Tab className="text-[#FBA403]" label="Home" />
+          <Tab className="text-white" label=" Getting a Taxi" />
+          <Tab className="text-white" label="Mobile App" />
+          <Tab className="text-white" label=" Contact Us" />
+        </Tabs>
+        {/* <List tabindex="0" className="flex ">
+          <ListItem role="link" sx={{ color: "#FBA403"  }}>
             Home
-          </Link>
-          <Link sx={{ color: "#ffffff" }} href="#" underline="none">
+          </ListItem>
+          <ListItem role="link" sx={{ color: "#ffffff" }} >
             Getting a Taxi
-          </Link>
-          <Link sx={{ color: "#ffffff" }} href="#" underline="none">
+          </ListItem>
+          <ListItem role="link" sx={{ color: "#ffffff" }} >
             Mobile App
-          </Link>
-          <Link sx={{ color: "#ffffff" }} href="#" underline="none">
+          </ListItem>
+          <ListItem role="link" sx={{ color: "#ffffff" }} >
             Contact Us
-          </Link>
-        </List>
+          </ListItem>
+        </List> */}
         <img src="header-icons.svg" alt="Notification and User icons"></img>
       </Toolbar>{" "}
     </AppBar>
