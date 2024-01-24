67c55fefbb70440a4029525709d8e3d8dcb1694c
will.terra <wpterra@outlook.com>
2024-01-23 21:30:02 -0300

    feat: home page added

--- src/components/BookMyRide.tsx
+++ src/components/BookMyRide.tsx
@@ -1,20 +1,86 @@
-import { Typography } from "@mui/material";
-import React from "react";
+import {
+  Box,
+  FormGroup,
+  FormLabel,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+  Button,
+} from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+import * as React from "react";
 
 const BookMyRide = () => {
   return (
-    <>
+    <Box className="bg-[#242424] flex justify-evenly gap-2 pt-36 pb-20">
       <img
         src="myRideYellowCar.svg"
         alt="a yellow car with the my ride logo on street"
+        width="610px"
       />{" "}
-      <Typography variant="h6" sx={{ textTransform: "uppercase" }}>
-        Need a ride
-      </Typography>
-      <Typography variant="h3" sx={{fontWeight: 700}}>
-        Book with <span style={{ color: "#FBA403", fontStyle: "italic", fontWeight: 500 }}>my</span><span style={{ color: "#FBA403", fontStyle: "italic", }}>RIDE </span> now!
-      </Typography>
-    </>
+      <Box className=" mt-20 ">
+        <Box className="mb-5">
+          <Typography
+            sx={{
+              textTransform: "uppercase",
+              color: "#FFFFFF",
+              fontSize: "26px",
+            }}
+          >
+            {" "}
+            Need a ride?
+          </Typography>
+          <Typography
+            sx={{ color: "#ffffff", fontSize: "50px", fontWeight: 700 }}
+          >
+            Book with{" "}
+            <span
+              style={{ color: "#FBA403", fontStyle: "italic", fontWeight: 500 }}
+            >
+              my
+            </span>
+            <span
+              style={{
+                color: "#FBA403",
+                fontStyle: "italic",
+                textTransform: "uppercase",
+              }}
+            >
+              ride{" "}
+            </span>{" "}
+            now!
+          </Typography>
+        </Box>
+
+        <FormGroup className="bg-[#2c2c2c] py-6 px-5 w-[610px]">
+          <FormLabel className="text-white font-bold text-base mb-2">
+            Find a ride now
+          </FormLabel>
+          <InputLabel sx={{ color: "#ffffff" }} id="your-pickup">
+            Your Pickup
+          </InputLabel>
+          <Select
+            className="text-white mb-2  border-white"
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={0}
+            label="your-pickup"
+          >
+            <MenuItem value={0}>Current Location </MenuItem>
+          </Select>
+          <TextField
+            className="text-white mb-2  border-white focus:border-[FBA403]"
+            sx={{ color: "#FFFFFF !important" }}
+            id="outlined-basic"
+            label="Your Destination"
+            variant="outlined"
+          />
+          <Button className="bg-[#FBA403] text-white"> <SearchIcon/> &nbsp; Find a driver</Button>
+        </FormGroup>
+      </Box>
+    </Box>
   );
 };
 
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [ "./index.html", "./src/**/*.{html,tsx}"],
+  important: "#root",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
